91c5b6b7362ebfadb045e40a4477db1b02e6f670
Ashley Towns <mail@ashleytowns.id.au>
2016-01-02 19:28:59 +1100

    move cchipmunk from local to git repo

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/aktowns/CSDL.git", Version(1,0,0)),
     .Package(url: "https://github.com/aktowns/CXML2.git", Version(1,0,0)),
-    .Package(url: "../CChipmunk2D/.git", Version(1,0,0))
+    .Package(url: "https://github.com/aktowns/CChipmunk2D.git", Version(1,0,0))
   ],
   targets: [
     Target(
