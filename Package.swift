c145a2c6adc97514940dd6a985d927710248b52d
Ashley Towns <mail@ashleytowns.id.au>
2016-01-03 23:18:48 +1100

    added tmx

--- Package.swift
+++ Package.swift
@@ -5,7 +5,8 @@ let package = Package(
   dependencies: [
     .Package(url: "https://github.com/aktowns/CSDL.git", Version(1,0,0)),
     .Package(url: "https://github.com/aktowns/CXML2.git", Version(1,0,0)),
-    .Package(url: "https://github.com/aktowns/CChipmunk2D.git", Version(1,0,0))
+    .Package(url: "https://github.com/aktowns/CChipmunk2D.git", Version(1,0,0)),
+    .Package(url: "https://github.com/aktowns/CTMX.git", Version(1,0,0))
   ],
   targets: [
     Target(
